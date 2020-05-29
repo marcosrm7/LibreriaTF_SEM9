760f13f51bfa7b005d211e9ff3f4d3be63069667
LeonardoZevallos <iupac19999@gmail.com>
2020-05-29 15:42:37 -0500

    Hola

--- src/main/java/pe/edu/upc/repository/IAccountRepository.java
+++ src/main/java/pe/edu/upc/repository/IAccountRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.entity.Account;
-
+/*Hola*/
 @Repository
 public interface IAccountRepository extends JpaRepository <Account, Integer> {
 
