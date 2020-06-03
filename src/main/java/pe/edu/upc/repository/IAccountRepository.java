ee4b1bb61f85c1982975cb274bacc55c9147fbed
LeonardoZevallos <iupac19999@gmail.com>
2020-06-03 12:03:37 -0500

    Avance

--- src/main/java/pe/edu/upc/repository/IAccountRepository.java
+++ src/main/java/pe/edu/upc/repository/IAccountRepository.java
@@ -1,11 +1,14 @@
 package pe.edu.upc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.entity.Account;
 
 @Repository
-public interface IAccountRepository extends JpaRepository <Account, Integer> {
-
+public interface IAccountRepository extends JpaRepository<Account, Integer> {
+	@Query("select count(a.nameAccount) from Account a where upper(a.nameAccount) = upper(:nameAccount)")
+	public int SearchCategory(@Param("nameAccount") String nombre);
 }
--- src/main/java/pe/edu/upc/repository/IExemplaryRepository.java
+++ src/main/java/pe/edu/upc/repository/IExemplaryRepository.java
@@ -1,11 +1,16 @@
 package pe.edu.upc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import pe.edu.upc.entity.Book;
 import pe.edu.upc.entity.Exemplary;
 
 @Repository
-public interface IExemplaryRepository extends JpaRepository <Exemplary, Integer> {
-
+public interface IExemplaryRepository extends JpaRepository <Exemplary, Integer>{
+	@Query("select count(e.exemplaryBook) from Exemplary a where upper(e.exemplaryBook)=upper(:exemplaryBook) and upper(e.countExemplary)=upper(:countExemplary)")
+	public int searchExemplary(@Param("exemplaryBook") Book ejemplar, @Param("countExemplary") int conteo);
 }
+
