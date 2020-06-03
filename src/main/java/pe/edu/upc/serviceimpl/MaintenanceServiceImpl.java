cbcd22f44159435e5ca9f0f74bd364a16f6b437d
alvaroaraujo1 <alvaro.0410410@gmail.com>
2020-06-03 13:15:16 -0500

    Creacion de servicios de mantenimiento

---
+++ src/main/java/pe/edu/upc/serviceimpl/MaintenanceServiceImpl.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.serviceimpl;
+
+import java.io.Serializable;
+
+import pe.edu.upc.serviceinterface.IMaintenanceService;
+
+public class MaintenanceServiceImpl implements Serializable,  IMaintenanceService{
+
+}
