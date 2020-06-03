ce4693d3079e06bff66d11626f28b20b65d1cb15
alvaroaraujo1 <alvaro.0410410@gmail.com>
2020-06-03 13:35:41 -0500

    Implementacion del service impl maintenance

--- src/main/java/pe/edu/upc/serviceimpl/MaintenanceServiceImpl.java
+++ src/main/java/pe/edu/upc/serviceimpl/MaintenanceServiceImpl.java
@@ -1,9 +1,33 @@
 package pe.edu.upc.serviceimpl;
 
 import java.io.Serializable;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pe.edu.upc.entity.Maintenance;
+import pe.edu.upc.repository.IMaintenanceRepository;
 import pe.edu.upc.serviceinterface.IMaintenanceService;
 
-public class MaintenanceServiceImpl implements Serializable,  IMaintenanceService{
+@Service
+public class MaintenanceServiceImpl implements Serializable, IMaintenanceService {
+
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private IMaintenanceRepository cR;
+
+	@Override
+	public void insert(Maintenance _maintenance) {
+		// TODO Auto-generated method stub
+		cR.save(_maintenance);
+	}
+
+	@Override
+	public List<Maintenance> list() {
+		// TODO Auto-generated method stub
+		return cR.findAll();
+	}
 
 }
