4ab7a7b5d5a9b0d69df1da17ce01c47b8984207d
alvaroaraujo1 <alvaro.0410410@gmail.com>
2020-06-03 13:40:36 -0500

    Implementacion de el maintenanceservice

--- src/main/java/pe/edu/upc/serviceinterface/IMaintenanceService.java
+++ src/main/java/pe/edu/upc/serviceinterface/IMaintenanceService.java
@@ -1,5 +1,12 @@
 package pe.edu.upc.serviceinterface;
 
+import java.util.List;
+
+import pe.edu.upc.entity.Maintenance;
+
 public interface IMaintenanceService {
+	public void insert(Maintenance _maintenance);
+
+	List<Maintenance> list();
 
 }
