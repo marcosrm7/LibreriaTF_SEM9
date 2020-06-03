16bd63f2e545ef0764875f924f2b960fc4cfb0ed
casquinobet <gonzalocasquino8@gmail.com>
2020-06-02 22:01:23 -0500

    Mostrar error por digitar caracteres especiales y numeros

--- src/main/java/pe/edu/upc/entity/Author.java
+++ src/main/java/pe/edu/upc/entity/Author.java
@@ -8,25 +8,32 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Entity
-@Table (name="Autor")
+@Table(name = "Autor")
 public class Author implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idAuthor;
-	@Column (name="nameAuthor", nullable=false, length=45)
+
+	@NotEmpty(message = "El nombre es obligatorio")
+	@Pattern(regexp = "[a-zA-Z]+", message = "El nombre solo puede tener letras")
+	@Column(name = "nameAuthor", nullable = false, length = 45)
 	private String nameAuthor;
-	@Column (name="lastnameAuthor", nullable=false, length=45)
+	@NotEmpty(message = "El apellido es obligatorio")
+	@Pattern(regexp = "[a-zA-Z]+", message = "El apellido solo puede tener letras")
+	@Column(name = "lastnameAuthor", nullable = false, length = 45)
 	private String lastnameAuthor;
-	
+
 	public Author() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public Author(int idAuthor, String nameAuthor, String lastnameAuthor) {
 		super();
 		this.idAuthor = idAuthor;
@@ -34,25 +41,28 @@ public class Author implements Serializable {
 		this.lastnameAuthor = lastnameAuthor;
 	}
 
-
 	public int getIdAuthor() {
 		return idAuthor;
 	}
+
 	public void setIdAuthor(int idAuthor) {
 		this.idAuthor = idAuthor;
 	}
+
 	public String getNameAuthor() {
 		return nameAuthor;
 	}
+
 	public void setNameAuthor(String nameAuthor) {
 		this.nameAuthor = nameAuthor;
 	}
+
 	public String getLastnameAuthor() {
 		return lastnameAuthor;
 	}
+
 	public void setLastnameAuthor(String lastnameAuthor) {
 		this.lastnameAuthor = lastnameAuthor;
 	}
-	
-	
+
 }
