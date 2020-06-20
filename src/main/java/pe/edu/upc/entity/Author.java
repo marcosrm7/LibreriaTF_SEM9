55fb8eba27b28ac428c6b8c90949dd0ec007235f
marcosrm7 <m2066r@gmail.com>
2020-06-20 10:02:41 -0500

    extras

--- src/main/java/pe/edu/upc/entity/Author.java
+++ src/main/java/pe/edu/upc/entity/Author.java
@@ -21,7 +21,8 @@ public class Author implements Serializable {
 	private int idAuthor;
 
 	// @NotEmpty(message = "El nombre es obligatorio")
-	@Pattern(regexp = "[a-zA-ZÀ-ÿ\\u00f1\\u00d1]+(\\s[a-zA-ZÀ-ÿ\\u00f1\\u00d1])*[a-zA-ZÀ-ÿ\\u00f1\\u00d1]+$", message = "El nombre solo puede tener letras")
+    @Pattern(regexp = "[a-zA-ZÀ-ÿ\\u00f1\\u00d1]+(\\s[a-zA-ZÀ-ÿ\\u00f1\\u00d1])*[a-zA-ZÀ-ÿ\\u00f1\\u00d1]+$", message = "El nombre solo puede tener letras")
+	
 	@Column(name = "nameAuthor", nullable = false, length = 45)
 	private String nameAuthor;
 
